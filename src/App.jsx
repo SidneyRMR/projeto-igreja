d39a3fa58ae9e93aa14ddc46abaf517c6c4b6b00
Sidney Rafael Mantovani Ribeiro <sidneyrafael191@gmail.com>
2022-09-06 04:11:55 -0300

    Commit Inicial

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+import Vendas from './Vendas/Vender'
+
+export default () => {
+  return (
+    <div className="App">
+      <Vendas/>
+    </div>
+  );
+}
---
+++ src/data/produtos.js
@@ -0,0 +1,11 @@
+export default [
+    {id:1, nome: "Pastel", preco: 8, tipo: "un"},
+    {id:2, nome: "Cachorro Quente", preco: 8, tipo: "un"},
+    {id:3, nome: "Milho", preco: 5, tipo: "un"},
+    {id:4, nome: "Pamonha", preco: 7, tipo: "un"},
+    {id:5, nome: "Lanche de Pernil", preco: 10, tipo: "un"},
+    {id:6, nome: "X-salada", preco: 10, tipo: "un"},
+    {id:7, nome: "Coca-cola", preco: 4, tipo: "un"},
+    {id:8, nome: "Guaraná", preco: 4, tipo: "un"},
+    {id:9, nome: "Cerveja", preco: 5, tipo: "un"}
+]
