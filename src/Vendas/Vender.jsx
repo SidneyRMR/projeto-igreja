d64f765d351aac1e7e4d780af3c6b9dbc3c9d8f8
Sidney Rafael Mantovani Ribeiro <sidneyrafael191@gmail.com>
2022-09-10 02:27:23 -0300

    modified:   src/Vendas/Vender.jsx

--- src/Vendas/Vender.jsx
+++ src/Vendas/Vender.jsx
@@ -6,8 +6,6 @@ export default props => {
             <a className="vender" href="/sangria">Sangria</a>
             <a className="vender" href="/cadastros">Cadastros</a><br />
             <a className="vender" href="/fechamento-geral">Fechamento</a>
-
-            
             <a className="vender" href="/vendas/pagamento">Pagamento</a><br />
             <a className="vender" href="/">Sair</a>
         </div>
