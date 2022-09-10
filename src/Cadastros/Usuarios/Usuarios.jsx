ad124c5ce7a75c46bc9f6c75ebc986e46111bb6a
Sidney Rafael Mantovani Ribeiro <sidneyrafael191@gmail.com>
2022-09-10 03:13:13 -0300

    listagem dos usuários completa

--- src/Cadastros/Usuarios/Usuarios.jsx
+++ src/Cadastros/Usuarios/Usuarios.jsx
@@ -1,7 +1,42 @@
 
+import Table from 'react-bootstrap/Table';
+import usuarios from '../../data/usuarios';
+
+
 export default function(props) {
 
-    return(
-        <h1>Tela de cadastro de usuarios</h1>
-    )
-}
+    function getUsuarios() {
+        return usuarios.map((usuario, i) => {
+            return (
+                <tr key={usuario.id}
+                    className={i % 2 === 0 ? 'Par' : 'Impar'}>
+                    <td >{usuario.nome}</td>
+                    <td>{usuario.login}</td>
+                    <td>{usuario.senha}</td>
+                    <td></td>
+                    <td><button >Alterar</button></td>
+                </tr>
+                  )
+                })
+            }
+
+
+        return (
+            <div className='borda'>
+            <Table striped bordered hover >
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Login</th>
+                        <th>Senha</th>
+                        <th>Administrador</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {getUsuarios()}
+                </tbody>
+            </Table>
+
+            </div>
+        )
+    }
