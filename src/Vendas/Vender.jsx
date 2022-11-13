729bcd9b4b354efcb7b74e4dbf280583f667df07
Sidney Rafael Mantovani Ribeiro <sidneyrafael191@gmail.com>
2022-11-13 04:24:43 -0300

    modified:   src/App.css 	modified:   src/Vendas/Vender.jsx

--- src/Vendas/Vender.jsx
+++ src/Vendas/Vender.jsx
@@ -1,12 +1,14 @@
 import produtos from '../data/produtos'
-import DropdownButton from 'react-bootstrap/DropdownButton';
 import Dropdown from 'react-bootstrap/Dropdown';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Table from 'react-bootstrap/Table';
 
 const Vender = () => {
+
+    // //Hook useState
+    // const produto = useState('')
+
+
 
     function getProdutos() {
         return produtos.map((produto, i) => {
@@ -20,30 +22,38 @@ const Vender = () => {
         })
     }
 
+
+
+
+
     return (
-        
-            <Container>
-                <Row>
-                    <Col>
-                        
-                            <DropdownButton className="menuPrincipal" title="Menu" variant="warning" size="lg">
-                                <Dropdown.Item href="/sangria">Sangria</Dropdown.Item>
-                                <Dropdown.Item href="/cadastros/produtos">Ver Produtos</Dropdown.Item>
-                                <Dropdown.Item href="/fechamento-caixa">Fechar Caixa</Dropdown.Item>
-                                <Dropdown.Item href="/">Sair</Dropdown.Item>
-                            </DropdownButton>
-                        
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs={6}><div className="title">Produtos</div>
-                        <div>
-                            {getProdutos()}
-                        </div>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
+
+        <Table>
+            <thead>
+                <tr>
+                    <th>Menu</th>
+                    <th>Vendas</th>
+                    <th>Pagamento</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+
+                        <Dropdown.Item href="/sangria">Sangria</Dropdown.Item>
+                        <Dropdown.Item href="/cadastros/produtos">Ver Produtos</Dropdown.Item>
+                        <Dropdown.Item href="/fechamento-caixa">Fechar Caixa</Dropdown.Item>
+                        <Dropdown.Item href="/">Sair</Dropdown.Item>
+
+                    </td>
+                    <td>
+                        <Col xs={6}><div className="title">Produtos</div>
+                            <div>
+                                {getProdutos()}
+                            </div>
+                        </Col>
+                    </td>
+                    <td>
                         <Table striped bordered hover size="sm">
                             <thead>
                                 <tr>
@@ -56,22 +66,21 @@ const Vender = () => {
                             </thead>
                             <tbody>
                                 <tr>
-                                    <td></td>
+                                    <td>#</td>
                                     <td>1</td>
-                                    <td>Pastel</td>
-                                    <td>10,00</td>
-                                    <td>10,00</td>
+                                    <td>Refri</td>
+                                    <td>8</td>
+                                    <td>8</td>
+                                 
                                 </tr>
                             </tbody>
                         </Table>
-                    </Col>
-                    <Col>
                         <a className="vender" href="/vendas/pagamento">Pagamento</a>
-                    </Col>
-                </Row>
+                    </td>
+                </tr>
+            </tbody>
+        </Table>
 
-            </Container>
-        
 
 
 
