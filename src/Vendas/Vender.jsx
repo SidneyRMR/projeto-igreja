d6b60517953c24a3a8a787568adb364f71cd04a1
Sidney Rafael Mantovani Ribeiro <sidneyrafael191@gmail.com>
2023-01-08 00:17:19 -0300

    ajute no vendas na formula novaCompra, somente para commitar sem bug

--- src/Vendas/Vender.jsx
+++ src/Vendas/Vender.jsx
@@ -196,28 +196,28 @@ const Vender = () => {
     //         console.log(error)
     //     }
     // }
-    // const novaCompra = async () => {
-    //     try {
+    const novaCompra = async () => {
+        // try {
 
-    //         const id_caixa = 
-    //         await axios.post('http://localhost:8800/compras', {
-    //             id_caixa: 
-    //     });
-    //     //   Loop pelos produtos da lista de compras
-    //       for (const produto of produtos) {
-    //         // Adiciona uma linha à tabela "tb_compras" para cada produto
-    //         await axios.post('http://localhost:8800/tb_compraprodutos', {
-    //             id_produto: produto.id,
-    //             quantidade_produto: produto.quantidade,
-    //             id_compra: 1,
-    //         });
-    //     }
+        //     const id_caixa = 
+        //     await axios.post('http://localhost:8800/compras', {
+        //         id_caixa: 
+        // });
+        // //   Loop pelos produtos da lista de compras
+        //   for (const produto of produtos) {
+        //     // Adiciona uma linha à tabela "tb_compras" para cada produto
+        //     await axios.post('http://localhost:8800/tb_compraprodutos', {
+        //         id_produto: produto.id,
+        //         quantidade_produto: produto.quantidade,
+        //         id_compra: 1,
+        //     });
+        // }
       
-    //       console.log(`Lista de compras salva com sucesso`);
-    //     } catch (error) {
-    //       console.log(error);
-    //     }
-    //   }
+        //   console.log(`Lista de compras salva com sucesso`);
+        // } catch (error) {
+        //   console.log(error);
+        // }
+      }
 
 
 
