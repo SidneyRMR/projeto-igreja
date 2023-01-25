4f55cf095525aa9b74ed76a64ca7d1f5b2c81707
Sidney Rafael Mantovani Ribeiro <sidneyrafael191@gmail.com>
2023-01-24 23:49:39 -0300

    ajuste no url da api

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const api = axios.create({
-    baseURL: 'http://api-production-9bce.up.railway.app'
+    baseURL: 'https://api-production-9bce.up.railway.app'
 
     // baseURL: process.env.REACT_APP_API_URL
 })
